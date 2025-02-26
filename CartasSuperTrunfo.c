da7c8141286427e34bdcd671f0e3d11d813ece0b
helenapozi <helenapozi28@gmail.com>
2025-02-26 18:15:03 +0000

    printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,63 @@
 #include <stdio.h>
 
 int main (){
-printf ("Super trunfo\n");
+printf ("Super trunfo - Países. \n");
+
+char estado;
+char codigodacarta;
+char nomedacidade;
+int população;
+float areaemkm;
+float PIB;
+int numeropontoturisticos;
+
+
+//insira o nome do estado
+printf("")
+
+//insira o codigo da carta
+
+//insira o nome da cidade
+
+//insira a população
+
+//area em km
+
+//insira o PIB
+
+
+//insira o numero de pontos turisticos
+
+
+
 
 return 0;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
